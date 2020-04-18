c8e7c36224f4e3c61ead1a680a451f33e236e2d2
Wellington Almeida <wpgalmeida@gmail.com>
2020-04-18 19:29:15 -0300

    Initial commit

---
+++ tests/test_tic_tac_toe.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    # valor informado maior = 3
+    def test_size_value_of_board(self):
+        self.fail()
+
+    # tabuleiro foi criado
+    def test_create_board(self):
+        self.fail()
---
+++ tic_tac_toy_python_playground/tic_tac_toe.py
@@ -0,0 +1,3 @@
+def create_board(size_board):
+    if size_board > 3:
+        print("erro")
