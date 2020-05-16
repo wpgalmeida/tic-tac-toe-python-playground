f8977696d5fe62c86bf4d6abb5cbbd5ab7a26bd4
Wellington Almeida <wpgalmeida@gmail.com>
2020-05-16 09:16:04 -0300

    Updated Models and Tests.

--- tic_tac_toe_python_playground/apps/core/models.py
+++ tic_tac_toe_python_playground/apps/core/models.py
@@ -2,44 +2,60 @@ import uuid
 
 from django.db import models
 
-# Create your models here.
 
-
-class Player(models.Model):
+class StandardModelMixin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at", "-updated_at"]
+
+
+class Player(StandardModelMixin):
+    name = models.CharField(max_length=100, blank=False)
+    birth = models.DateField()
+    gender = models.CharField(max_length=1, blank=False)  # Male, Female, Others
     bot = models.BooleanField(default=False)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
-class Board(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Board(StandardModelMixin):
     num_cols = models.IntegerField()
     num_rows = models.IntegerField()
 
 
-class PlayerBoard(models.Model):
+class PlayerBoard(StandardModelMixin):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    symbol = models.CharField(max_length=1)
+    symbol = models.CharField(max_length=1, blank=False, default=None)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["player", "board"], name="unique_player_board"
+            )
+        ]
 
 
-class Game(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Game(StandardModelMixin):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    draw = models.BooleanField(default=False)
 
 
-class Movements(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+class Movements(StandardModelMixin):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     position = models.IntegerField()
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["position", "board"], name="unique_position_board"
+            )
+        ]
 
-class PlayerGame(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    winner = models.BooleanField(default=False)
-    draw = models.BooleanField(default=False)
