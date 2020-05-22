9f0782a96b01b1e81b61b960e7f5360b1244eb21
Wellington Almeida <wpgalmeida@gmail.com>
2020-05-22 17:47:08 -0300

    Update Admin to display table Board

--- tic_tac_toe_python_playground/apps/core/admin.py
+++ tic_tac_toe_python_playground/apps/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from tic_tac_toe_python_playground.apps.core.models import Player
+from tic_tac_toe_python_playground.apps.core.models import Player, Board
 
 admin.site.register(Player)
+admin.site.register(Board)
