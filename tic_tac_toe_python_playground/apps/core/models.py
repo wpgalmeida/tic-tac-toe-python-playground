aa1cc4e6ffb7cec9aff57c3364edc28a0c26a302
Wellington Almeida <wpgalmeida@gmail.com>
2020-07-20 07:52:57 -0300

    Criação dos Models (Tabelas) para o jogo da velha.

--- tic_tac_toe_python_playground/apps/core/models.py
+++ tic_tac_toe_python_playground/apps/core/models.py
@@ -1,3 +1,61 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class StandardModelMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at", "-updated_at"]
+
+
+class Player(StandardModelMixin):
+    name = models.CharField(max_length=100, blank=False)
+    birth = models.DateField()
+    gender = models.CharField(max_length=1, blank=False)  # Male, Female, Others
+    bot = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Board(StandardModelMixin):
+    num_cols = models.IntegerField()
+    num_rows = models.IntegerField()
+
+
+class PlayerBoard(StandardModelMixin):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=1, blank=False, default=None)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["player", "board"], name="unique_player_board"
+            )
+        ]
+
+
+class Game(StandardModelMixin):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    draw = models.BooleanField(default=False)
+
+
+class Movements(StandardModelMixin):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    position = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["position", "board"], name="unique_position_board"
+            )
+        ]
