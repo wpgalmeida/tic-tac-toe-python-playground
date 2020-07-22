72ab81b22f4e78d72cf483be324f91b572f8a9b7
Wellington Almeida <wpgalmeida@gmail.com>
2020-07-22 08:23:38 -0300

    Criação de mais testes para tabela "Players"

--- tests/apps/core/test_models.py
+++ tests/apps/core/test_models.py
@@ -1,24 +1,59 @@
 from datetime import datetime
 
+from django.db import IntegrityError
 from django.test import TestCase
 
 from tic_tac_toe_python_playground.apps.core.models import Player
 
 
 class Test(TestCase):
+    def setUp(self) -> None:
+        self.name = "Augusto Carrara"
+        str_birth = "2020-01-01"
+        self.birth = datetime.strptime(str_birth, "%Y-%m-%d").date()
+        self.gender = "M"
+
+    # Tests for player
     def test_should_create_player(self):
-        name = "Augusto Carrara"
-        birth = "2020-01-01"
-        converted_birth = datetime.strptime(birth, "%Y-%m-%d").date()
-        gender = "M"
-        bot = False
         expected_count_value = 1
 
-        Player.objects.create(name=name, birth=birth, gender=gender, bot=bot)
-        created_player: Player = Player.objects.first()
+        Player.objects.create(name=self.name, birth=self.birth, gender=self.gender)
+        created_player = Player.objects.first()
 
         self.assertEqual(Player.objects.all().count(), expected_count_value)
-        self.assertEqual(created_player.name, name)
-        self.assertEqual(created_player.birth, converted_birth)
-        self.assertEqual(created_player.gender, gender)
-        self.assertEqual(created_player.bot, bot)
+        self.assertEqual(created_player.name, self.name)
+        self.assertEqual(created_player.birth, self.birth)
+        self.assertEqual(created_player.gender, self.gender)
+        self.assertFalse(created_player.bot)
+
+    def test_should_create_player_bot(self):
+        expected_count_value = 1
+
+        Player.objects.create(
+            name=self.name, birth=self.birth, gender=self.gender, bot=True
+        )
+        created_player = Player.objects.first()
+
+        self.assertEqual(Player.objects.all().count(), expected_count_value)
+        self.assertEqual(created_player.name, self.name)
+        self.assertEqual(created_player.birth, self.birth)
+        self.assertEqual(created_player.gender, self.gender)
+        self.assertTrue(created_player.bot)
+
+    def test_should_raise_given_player_without_name(self):
+        with self.assertRaisesMessage(
+            IntegrityError, "NOT NULL constraint failed: core_player.name"
+        ):
+            Player.objects.create(name=None, birth=self.birth, gender=self.gender)
+
+    def test_should_raise_given_player_without_gender(self):
+        with self.assertRaisesMessage(
+            IntegrityError, "NOT NULL constraint failed: core_player.gender"
+        ):
+            Player.objects.create(name=self.name, birth=self.birth, gender=None)
+
+    def test_should_raise_given_player_without_birth(self):
+        with self.assertRaisesMessage(
+            IntegrityError, "NOT NULL constraint failed: core_player.birth"
+        ):
+            Player.objects.create(name=self.name, gender=self.gender)
