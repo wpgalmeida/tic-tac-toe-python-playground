fc94445fe368c87eb2b96c80e2c204851f8ff5a9
Wellington Almeida <wpgalmeida@gmail.com>
2021-08-12 19:14:27 -0300

    new test board

--- tests/apps/test_game_builder.py
+++ tests/apps/test_game_builder.py
@@ -12,3 +12,8 @@ class Test(TestCase):
         expected_board_to_be_tested = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
         result = create_board()
         self.assertEquals(expected_board_to_be_tested, result)
+
+    def test_should_create_default_board_4x4(self):
+        expected_board_to_be_tested = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+        result = create_board(4)
+        self.assertEquals(expected_board_to_be_tested, result)
